0b3c7a9530161d40c32106605bb51712fa3bacd6
davidterbeek <david.ter.beek@hva.nl>
2019-04-08 15:21:09 +0200

    tabitems toegevoegd en gekoppeld aan menu

--- ShinyApp.R
+++ ShinyApp.R
@@ -18,14 +18,16 @@ ui <- dashboardPage(
   dashboardHeader(
     title = "bruto binnenlands product tegen de uitstoot van broeikasgassen per capita"
   ),
+
+#-------------------------- DASHBOARD SIDEBAR ------------------------------------------    
   dashboardSidebar(
     sidebarMenu(menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
                 
                 menuItem("Charts", tabName = "Plots", icon = icon("bar-chart-o"),
-                         menuSubItem("1D - Histogram", tabName = "Histogram", icon = icon("angle-double-right")),
-                         menuSubItem("2D - Lijngrafieken", tabName = "Lijngrafieken", icon = icon("angle-double-right")),
-                         menuSubItem("2D - Dumbell charts", tabName = "Dumbell charts", icon = icon("angle-double-right")),
-                         menuSubItem("3D - Geospatiale plots", tabName = "Geospatiale plots", icon = icon("angle-double-right"))
+                         menuSubItem("1D - Histogrammen", tabName = "histogrammen", icon = icon("angle-double-right")),
+                         menuSubItem("2D - Lijngrafieken", tabName = "lijngrafieken", icon = icon("angle-double-right")),
+                         menuSubItem("2D - Dumbell charts", tabName = "dumbellcharts", icon = icon("angle-double-right")),
+                         menuSubItem("3D - Geospatiale visualisaties", tabName = "geospatialevisualisaties", icon = icon("angle-double-right"))
                          ),
                 
                          
@@ -38,28 +40,106 @@ ui <- dashboardPage(
                 selectInput("countryInput", "Country",
                             choices = c("CANADA", "FRANCE", "ITALY")))
   ),
+
+#-------------------------- DASHBOARD BODY ------------------------------------------  
   dashboardBody(
-    fluidRow(
-      box(
-        title = "Histogram", status = "primary", solidHeader = TRUE,
-        collapsible = TRUE,
-        plotOutput("plot3", height = 250)
-      )
-    )
     tabItems(
-      tabItem(tabName = "dashboard",
-              h2("Dashboard tab content")
+      
+      ##------------------- TAB ITEM 1 - HISTOGRAMMEN ---------------------
+      tabItem(
+        tabName = "histogrammen",
+        
+          fluidRow(
+            
+            # BOX 1 - Histogram GDP
+            box(
+              title = "Histogram GDP", status = "primary", solidHeader = TRUE,
+              collapsible = TRUE,
+              plotOutput("output$hist.gdp", height = 250)
+            ),
+            
+            # BOX 2 - Histgram Broeikasgassen
+            box(
+              title = "Histogram Broeikasgassen", status = "primary", solidHeader = TRUE,
+              collapsible = TRUE,
+              plotOutput("output$hist.gdp", height = 250)
+            )
+          )
+        ),
+      
+      ##----------------- TAB ITEM 2 - LIJNGRAFIEKEN --------------
+      tabItem(
+        tabName = "lijngrafieken",
+        
+        fluidRow(
+          
+          # BOX 1 - lijngrafieken GDP
+          box(
+            title = "Lijngrafieken GDP", status = "primary", solidHeader = TRUE,
+            collapsible = TRUE,
+            plotOutput("output$hist.gdp", height = 250)
+          ),
+          
+          # BOX 2 - lijngrafieken Broeikasgassen
+          box(
+            title = "Lijngrafieken Broeikasgassen", status = "primary", solidHeader = TRUE,
+            collapsible = TRUE,
+            plotOutput("output$hist.gdp", height = 250)
+          )
+        )
       ),
       
-      tabItem(tabName = "widgets",
-              h2("Widgets tab content")
+      ##--------------- TAB ITEM 3 - DUMBELL CHARTS ---------------
+      tabItem(
+        tabName = "dumbellcharts",
+        
+        fluidRow(
+          
+          # BOX 1 - dumbell charts GDP
+          box(
+            title = "Dumbell charts GDP", status = "primary", solidHeader = TRUE,
+            collapsible = TRUE,
+            plotOutput("output$hist.gdp", height = 250)
+          ),
+          
+          # BOX 2 - dumbell charts Broeikasgassen
+          box(
+            title = "Dumbell charts Broeikasgassen", status = "primary", solidHeader = TRUE,
+            collapsible = TRUE,
+            plotOutput("output$hist.gdp", height = 250)
+          )
+        )
+      ),
+      
+      ##--------------- TAB ITEM 4 - GEOSPATIALE VISUALISATIES -------------
+      tabItem(
+        tabName = "geospatialevisualisaties",
+        
+        fluidRow(
+          
+          # BOX 1 - Geospatiale GDP
+          box(
+            title = "Geospatiale GDP", status = "primary", solidHeader = TRUE,
+            collapsible = TRUE,
+            plotOutput("output$hist.gdp", height = 250)
+          ),
+          
+          # BOX 2 - Geospatiale Broeikasgassen
+          box(
+            title = "Geospatiale Broeikasgassen", status = "primary", solidHeader = TRUE,
+            collapsible = TRUE,
+            plotOutput("output$hist.gdp", height = 250)
+          )
+        )
       )
     )
   )
 )
+  
 
 
-#------------------------------------------------------------------------------------------------------------------
+
+#-------------------------- SERVER INPUT & OUTPUT-----------------------------------------------------------------------
 server <- function(input, output) {
   output$hist.gdp <- renderPlot({
     gasGdpCapita %>% 
