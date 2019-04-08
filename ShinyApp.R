d50fb524fc13eb156da0e12def679e851c32d4bd
rudy.nutbeij <Rudy.Nutbeij@topicus.nl>
2019-04-08 17:27:27 +0200

    d

--- ShinyApp.R
+++ ShinyApp.R
@@ -2,31 +2,51 @@
 #if you do, it simply loads the package
 if(!require(pacman))install.packages("pacman")
 
-devtools::install_github('HvAMinor/bbplot')
+devtools::install_github('HvAMinor/bbplot', upgrade = c("never"))
+pacman::p_load('extrafont')
+
+extrafont::font_import(prompt = FALSE, pattern = "*erdana*")
+extrafont::loadfonts(device="win")
 
 pacman::p_load('rstudioapi','dplyr', 'tidyr', 'gapminder',
-               'ggplot2',  'ggalt', 'eurostat',
-               'forcats', 'R.utils', 'png', 'glue', 'countrycode', 
+              'ggalt', 'eurostat', 'colorspace', 'forcats', 'R.utils', 'png', 'glue', 'countrycode', 
                'grid', 'ggpubr', 'scales')
 
+library(ggplot2)
 library(bbplot)
 library(shiny)
 library(shinydashboard)
-#windowsFonts("Arial" = windowsFont("Arial"))
+
 # the following line is for getting the path of your current open file
 current_path <- getActiveDocumentContext()$path
 # The next line set the working directory to the relevant one:
 setwd(dirname(current_path ))
 # you can make sure you are in the right directory
 print(getwd())
+bronnen <-"Eurostat (2019) Real GDP per capita [Data file] Retrieved from: https://ec.europa.eu/eurostat/web/products-datasets/-/sdg_08_10 \r\nEurostat (2019) Greenhouse gas emissions per capita [Data file] Retrieved from: https://ec.europa.eu/eurostat/web/products-datasets/-/t2020_rd300"
 
 gasGdpCapita <- read.csv('gasGdpCapita.csv', row.names='X', header=TRUE, stringsAsFactors = FALSE) %>%
   mutate(geo=countrycode::countrycode(geo, origin='eurostat', destination='country.name'))
-bronnen <-"Eurostat (2019) Real GDP per capita [Data file] Retrieved from: https://ec.europa.eu/eurostat/web/products-datasets/-/sdg_08_10 \r\nEurostat (2019) Greenhouse gas emissions per capita [Data file] Retrieved from: https://ec.europa.eu/eurostat/web/products-datasets/-/t2020_rd300"
 
 dumbell <- gasGdpCapita %>% 
     select(geo, jaar, gas) %>% 
     spread(jaar, gas, convert=TRUE)
+
+geodata_N0 <- get_eurostat_geospatial(year = "2016",
+                                      nuts_level = 0,
+                                      resolution = "60")
+
+ # TOEVOEGEN - COUNTRY
+geodata_N0 <- geodata_N0 %>%
+  mutate(geo = countrycode(sourcevar = CNTR_CODE,
+                               origin = 'eurostat',
+                               destination = 'country.name'))
+
+
+ # DATASETS JOINEN - geodata_N0, gasGdpCapita
+map_data <- gasGdpCapita %>%
+  inner_join(y = geodata_N0, by = "geo")
+
 
 ui <- dashboardPage(
   dashboardHeader(
@@ -44,7 +64,7 @@ ui <- dashboardPage(
                 
                          
                 sliderInput("keuzejaren", "keuzejaren", 2000, 2016, c(2006, 2016)),
-                selectInput("keuzejaar", "keuzejaar", choices = seq(2000, 2016)),
+                selectInput("keuzejaar", "keuzejaar", choices = seq(2000, 2016), selected = 2006),
                 selectInput("klassen", "klassen", choices = seq(2, 8))
                 )
   ),
@@ -56,9 +76,16 @@ ui <- dashboardPage(
         plotOutput("dumbell", height = 750)
       ),
       box(
-        title = "lijn", status = "primary", solidHeader = TRUE,
+        title = "lijnplot", status = "primary", solidHeader = TRUE,
         collapsible = TRUE,
         plotOutput("lijn", height = 750)
+      )
+    ),
+    fluidRow(
+      box(
+        title = "geoplot", status = "primary", solidHeader = TRUE,
+        collapsible = TRUE,
+        plotOutput("geoplot", height = 750)
       )
     ),
     tabItems(
@@ -75,16 +102,39 @@ ui <- dashboardPage(
 
 
 server <- function(input, output) {
+  output$geoplot <- renderPlot({
+    klassen <- as.character(input$klassen)
+    keuzejaar <- as.character(input$keuzejaar)
+    map_data_gas <- map_data %>%
+    mutate(cat = cut_to_classes(x = gas, n = klassen, style = "quantile" ))
+
+    map_data_gas.plot <- ggplot(data = map_data_gas) +
+        geom_sf(data = geodata_N0, fill = "gray85", alpha = 1) +
+        geom_sf(data = map_data_gas[map_data_gas$jaar == keuzejaar,], 
+                aes(fill = cat),
+                color = "gray40", size = 0.2) +
+        geom_sf(data = geodata_N0, color = "gray30", size = 0.703, alpha = 0) +
+        coord_sf(xlim = c(-25, 45), ylim = c(35, 72)) +
+        scale_fill_discrete_sequential(name = "<AANVULLEN>", "ag_GrnYl") +
+        theme(legend.position = "left", legend.title.align = 0) +
+        guides(fill = guide_legend(reverse = TRUE)) +
+        labs(title = paste("<AANVULLEN>"),
+             subtitle = paste("<AANVULLEN>"),
+             caption = "<AANVULLEN>",
+             x = "Lengtegraad",
+             y = "Breedtegraad") +
+        bbc_style()
+    finalise_plot(plot_name = map_data_gas.plot,
+            source_name = bronnen,
+            save_filepath = glue("geo {keuzejaar}.png"),
+            width_pixels = 750, height_pixels = 600, logo_image_path = "dataloog.png")
+  })
+  
   output$lijn <- renderPlot({
     klassen <- as.character(input$klassen)
     keuzejaar.min <- as.character(min(input$keuzejaren))
     keuzejaar.max <- as.character(max(input$keuzejaren))
-    
-    # LIJNGRAFIEK LABS
-    dumbell.broeikas.titel <- glue("gemiddelde uitstoot broeikasgassen per hoofd vangdp {klassen} uitstootgroepen {keuzejaar.min} - {keuzejaar.max}.png")
-    dumbell.broeikas.x.as <- glue("Landsnamen")
-    dumbell.broeikas.y.as <- glue("Uitstoot broeikasgassen (gementen in tonnen CO2 equivalent)")
-    
+
     lijn.data <- gasGdpCapita %>% 
       filter(jaar %in% seq(keuzejaar.min, keuzejaar.max)) %>%
       mutate(range_gdp = cut_to_classes(gdp, n=klassen, style="quantile", decimals=0)) %>%
@@ -97,12 +147,7 @@ server <- function(input, output) {
     lijn.plot <- ggplot(lijn.data, aes(x = jaar, y = gas, colour = gdp)) +
       geom_line(size = 1) +
       geom_hline(yintercept = 0, size = 1, colour="#333333") +
-      bbc_style() + 
-      
-      # LABS KOPPELEN
-      labs(title=lijn.titel,
-           y = "",
-           ) +
+      bbc_style()
       
       # LEGENDA TITEL
       theme(legend.position = "right")
@@ -129,7 +174,7 @@ server <- function(input, output) {
                     size = 3,
                     colour_x = "#FAAB18",
                     colour_xend = "#1380A1") +
-      labs(title=dumbell.broeikas.title) +
+      labs(title=dumbell.broeikas.titel) +
       bbc_style()
 
     finalise_plot(plot_name = dumbell.plot,
--- test/test_stacked.R
+++ test/test_stacked.R
@@ -5,7 +5,7 @@ devtools::install_github('HvAMinor/bbplot')
 pacman::p_load('rstudioapi','dplyr', 'tidyr', 'gapminder',
                'ggplot2',  'ggalt', 'eurostat',
                'forcats', 'R.utils', 'png', 'glue', 
-               'grid', 'ggpubr', 'scales',
+               'grid', 'ggpubr', 'scales', 'colorspace',
                'bbplot')
 
 windowsFonts("Helvetica" = windowsFont("Helvetica Light"))
@@ -26,29 +26,125 @@ keuzejaar <- "2006"
 keuzejaar.min <- "2006"
 keuzejaar.max <- "2016"
 bronnen <-" Eurostat (2019) Real GDP per capita [Data file] Retrieved from: https://ec.europa.eu/eurostat/web/products-datasets/-/sdg_08_10 \r\n Eurostat (2019) Greenhouse gas emissions per capita [Data file] Retrieved from: https://ec.europa.eu/eurostat/web/products-datasets/-/t2020_rd300"
-
-stacked.gdp <- gasGdpCapita %>% 
-  filter(jaar %in% seq(keuzejaar.min, keuzejaar.max)) %>%
-  mutate(range_gdp = cut_to_classes(gdp, n=klassen, style="quantile", decimals=0)) %>%
-  dplyr::select(jaar, gdp=range_gdp, gas) %>%
-  group_by(gdp, jaar) %>% 
-  summarize(gas = mean(gas), count = n()) %>%
-  ungroup() %>%
-  dplyr::select(jaar, gdp, gas, count)
-
 ######### hieronder staan geen instellingen meer
+geodata_N0 <- get_eurostat_geospatial(year = "2016",
+                                      nuts_level = 0,
+                                      resolution = "60")
+
+ # TOEVOEGEN - COUNTRY
+geodata_N0 <- geodata_N0 %>%
+  mutate(country = countrycode(sourcevar = CNTR_CODE,
+                               origin = 'eurostat',
+                               destination = 'country.name') )
+
+
+ # SELECTEREN - GEOMETRY, GEO
+geodata_N0 <- geodata_N0 %>%
+  select("geometry","geo", "country")
+
+
+ # DATASETS JOINEN - geodata_N0, gasGdpCapita
+map_data <- gasGdpCapita %>%
+  inner_join(y = geodata_N0, by = "geo")
+
+
+ # DATAFRAME MAP_DATA_GDP (KOLOM BRUTO BINNENLANDS PRODUCT PER CAPITA) 
+map_data_gas <- map_data %>%
+  dplyr::select(-gdp) %>%
+  mutate(cat = cut_to_classes(x = gas, 
+                              n = 7,
+                              style = "quantile" ))
+
+
+ggplot(data = map_data_gas) +
   
+  # LANDEN EEN ONDERLIGGENDE GRIJZE KLEUR GEVEN (LANDEN ZONDER DATA WORDEN GRIJS)
+  geom_sf(data = geodata_N0,
+          fill = "gray85",
+          alpha = 1) +
+  
+  # LANDEN KLEUREN OP BASIS VAN CATEGORIE
+  geom_sf(data = map_data_gas[map_data_gas$jaar %in% c(2010),],
+          aes(fill = cat),
+          color = "gray40",
+          size = 0.2) +
+  
+  # RANDEN VAN LANDEN KLEUREN OP BASIS VAN GRENZEN
+  geom_sf(data = geodata_N0,
+          color = "gray30",
+          size = 0.703,
+          alpha = 0) +
+  
+  # CO?RDINATEN VAN DE MAP VERKLEINEN
+  coord_sf(xlim = c(-25, 45),
+           ylim = c(35, 72)) +
+  
+  # PASSEND KLEURENPALETTE, ZODAT ALLE KLASSEN DUIDELIJK ZICHTBAAR WORDEN
+  scale_fill_discrete_sequential(name = "<AANVULLEN>", "ag_GrnYl") +
+  
+  # POSITIE VAN LEGENDA NAAR LINKS
+  theme(legend.position = "left",
+        legend.title.align = 0) +
+  
+  # VOLGORDE VAN LEGENDA - HOOG > LAAG
+  guides(fill = guide_legend(reverse = TRUE))+
+  
+  # TITEL, ONDERTITEL, CAPTION, X-AS, Y-AS, LEGENDATITEL
+  labs(title = paste("<AANVULLEN>"),
+       subtitle = paste("<AANVULLEN>"),
+       caption = "<AANVULLEN>",
+       x = "Lengtegraad",
+       y = "Breedtegraad")
 
 
-) +
-  geom_bar(stat="identity",position="fill", aes(y=prop.gas))
+ # DATAFRAME MAP_DATA_GDP (KOLOM BRUTO BINNENLANDS PRODUCT PER CAPITA) 
+map_data_gdp <- map_data %>%
+  select(-gas) %>%
+  mutate(cat = cut_to_classes(x = gdp, 
+                              n = 7,
+                              style = "quantile" ))
 
-ggplot(stacked.gdp, aes(x=jaar, y=prop.gas, fill=gas)) +
-  geom_area(alpha=0.6, size=1, colour="black")
 
-ggplot(stacked.gdp, aes(x=jaar, y=prop.gas, fill=gdp)) +
-  geom_area(alpha=0.6, size=1, colour="black")
+ # PLOT MAP_DATA_GDP
+ggplot(data = map_data_gdp) +
+  
+  # LANDEN EEN ONDERLIGGENDE GRIJZE KLEUR GEVEN (LANDEN ZONDER DATA WORDEN GRIJS)
+  geom_sf(data = geodata_N0,
+          fill = "gray85",
+          alpha = 1) +
+  
+  # LANDEN KLEUREN OP BASIS VAN CATEGORIE
+  geom_sf(data = map_data_gdp[map_data_gdp$jaar %in% c(2010),],
+          aes(fill = cat),
+          color = "gray40",
+          size = 0.2) +
+  
+  # RANDEN VAN LANDEN KLEUREN OP BASIS VAN GRENZEN
+  geom_sf(data = geodata_N0,
+          color = "gray30",
+          size = 0.703,
+          alpha = 0) +
+  
+  coord_sf(xlim = c(-25, 45),
+           ylim = c(35, 72)) +
+  
+  
+  # PASSEND KLEURENPALETTE, ZODAT ALLE KLASSEN DUIDELIJK ZICHTBAAR WORDEN
+  scale_fill_discrete_sequential(name = "<AANVULLEN>", "ag_GrnYl") +
+  
+  # POSITIE VAN LEGENDA NAAR LINKS
+  theme(legend.position = "left",
+        legend.title.align = 0) +
+  
+  # VOLGORDE VAN LEGENDA - HOOG > LAAG
+  guides(fill = guide_legend(reverse = TRUE))+
+  
+  # TITEL, ONDERTITEL, CAPTION, X-AS, Y-AS, LEGENDATITEL
+  labs(title = paste("<AANVULLEN>"),
+       subtitle = paste("<AANVULLEN>"),
+       caption = "<AANVULLEN>",
+       x = "Lengtegraad",
+       y = "Breedtegraad")
 
-  geom_area(alpha=0.3, aes(x=jaar, y=prop.gas, colour="black", fill=gdp)) +
-  bbc_style()
+
   
