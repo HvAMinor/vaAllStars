1481ff4441f1899575ec4dddf51f79a6b0c19fb3
rudy.nutbeij <Rudy.Nutbeij@topicus.nl>
2019-04-08 15:59:40 +0200

    a

--- ShinyApp.R
+++ ShinyApp.R
@@ -12,9 +12,9 @@ pacman::p_load('rstudioapi','dplyr', 'tidyr', 'gapminder',
 library(bbplot)
 library(shiny)
 library(shinydashboard)
-windowsFonts("Arial" = windowsFont("Arial"))
+#windowsFonts("Arial" = windowsFont("Arial"))
 # the following line is for getting the path of your current open file
-current_path <- getActiveDocumentContext()$path 
+current_path <- getActiveDocumentContext()$path
 # The next line set the working directory to the relevant one:
 setwd(dirname(current_path ))
 # you can make sure you are in the right directory
@@ -22,7 +22,7 @@ print(getwd())
 
 gasGdpCapita <- read.csv('gasGdpCapita.csv', row.names='X', header=TRUE, stringsAsFactors = FALSE) %>%
   mutate(geo=countrycode::countrycode(geo, origin='eurostat', destination='country.name'))
-bronnen <-" Eurostat (2019) Real GDP per capita [Data file] Retrieved from: https://ec.europa.eu/eurostat/web/products-datasets/-/sdg_08_10 \r\n Eurostat (2019) Greenhouse gas emissions per capita [Data file] Retrieved from: https://ec.europa.eu/eurostat/web/products-datasets/-/t2020_rd300"
+bronnen <-"Eurostat (2019) Real GDP per capita [Data file] Retrieved from: https://ec.europa.eu/eurostat/web/products-datasets/-/sdg_08_10 \r\nEurostat (2019) Greenhouse gas emissions per capita [Data file] Retrieved from: https://ec.europa.eu/eurostat/web/products-datasets/-/t2020_rd300"
 
 dumbell <- gasGdpCapita %>% 
     select(geo, jaar, gas) %>% 
@@ -79,7 +79,12 @@ server <- function(input, output) {
     klassen <- as.character(input$klassen)
     keuzejaar.min <- as.character(min(input$keuzejaren))
     keuzejaar.max <- as.character(max(input$keuzejaren))
-    lijn.titel <- glue("gemiddelde gdp {klassen} uitstootgroepen {keuzejaar.min} - {keuzejaar.max}.png")
+    
+    # LIJNGRAFIEK LABS
+    dumbell.broeikas.titel <- glue("gemiddelde uitstoot broeikasgassen per hoofd vangdp {klassen} uitstootgroepen {keuzejaar.min} - {keuzejaar.max}.png")
+    dumbell.broeikas.x.as <- glue("Landsnamen")
+    dumbell.broeikas.y.as <- glue("Uitstoot broeikasgassen (gementen in tonnen CO2 equivalent)")
+    
     lijn.data <- gasGdpCapita %>% 
       filter(jaar %in% seq(keuzejaar.min, keuzejaar.max)) %>%
       mutate(range_gdp = cut_to_classes(gdp, n=klassen, style="quantile", decimals=0)) %>%
@@ -93,6 +98,13 @@ server <- function(input, output) {
       geom_line(size = 1) +
       geom_hline(yintercept = 0, size = 1, colour="#333333") +
       bbc_style() + 
+      
+      # LABS KOPPELEN
+      labs(title=lijn.titel,
+           y = "",
+           ) +
+      
+      # LEGENDA TITEL
       theme(legend.position = "right")
 
       finalise_plot(plot_name = lijn.plot,
@@ -104,7 +116,12 @@ server <- function(input, output) {
     keuzejaar.min <- as.character(min(input$keuzejaren))
     keuzejaar.max <- as.character(max(input$keuzejaren))
     dumbell.data <- dumbell %>% mutate(gap = dumbell[,keuzejaar.max] - dumbell[,keuzejaar.min])
-    dumbell.title <- glue("verandering uitstoot broeikasgassen per hoofd bevolking {keuzejaar.min} - {keuzejaar.max}")
+    
+    # DUMBELL LABS
+    dumbell.broeikas.titel <- glue("verandering uitstoot broeikasgassen per hoofd bevolking {keuzejaar.min} - {keuzejaar.max}")
+    dumbell.broeikas.x.as <- glue("Landsnamen")
+    dumbell.broeikas.y.as <- glue("Uitstoot broeikasgassen (gementen in tonnen CO2 equivalent)")
+    
     dumbell.plot <- ggplot(dumbell.data, aes(x = dumbell.data[,keuzejaar.max],
                                              xend = dumbell.data[,keuzejaar.min],
                                              y = reorder(geo, gap), group = geo)) +
@@ -112,7 +129,7 @@ server <- function(input, output) {
                     size = 3,
                     colour_x = "#FAAB18",
                     colour_xend = "#1380A1") +
-      labs(title=dumbell.title) +
+      labs(title=dumbell.broeikas.title) +
       bbc_style()
 
     finalise_plot(plot_name = dumbell.plot,
